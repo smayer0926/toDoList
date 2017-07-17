2d71c174dd007e73366094130d7e741cc1b9dfe0
Evan Johnson and Stephanie Mayer <evan@>
2017-07-17 15:29:35 -0700

    add multiple forms

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,30 @@
 //Business Logic
+function info(task,date, notes) {
+  this.task = task;
+  this.date = date;
+  this.notes = notes;
+}
+
+info.prototype.taskList = function() {
+  return this.task + ' must be done by: ' + this.date;
+}
 
 //User Interface
 $(document).ready(function() {
   $('#inputs').submit(function(event) {
     event.preventDefault();
-    var addList = $('#toDoList').val();
+    var task = $('#toDoList').val();
+    var dates = $('#date').val();
+    var notes = $('#note').val();
+    var newInfo  = new info(task,dates,notes);
 
-    $('#outputs').append('<li><input type="checkbox" name="addList" value="addList">' + addList);
+    $('#outputs').append('<li><input type="checkbox" name="addList" value="addList">' + " " + newInfo.taskList() + '<p><span class= "notes">See notes</span></p><p><span id="noteContent"></span></p></li>');
+
+
+
+    $('.notes').last().click(function() {
+      $('.notes').append(newInfo.notes);
+    });
 
     $('.form-control').val('');
   });
